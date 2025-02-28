d92f7533fb3151d3b10b58b38419076c494322bf
BahresAtli <bahresatli@hotmail.com>
2025-02-28 17:38:34 +0300

    Update and Add Column functions are added (some needs to be revised later)

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -30,6 +30,13 @@ class DatabaseHelper {
     return await db.insert(table, row);
   }
 
+  Future<int> update(Map<String, dynamic> row, String table) async {
+    final dbHelper = DatabaseHelper.instance;
+    final db = await dbHelper.database;
+    int id = row['recordID'];
+    return await db.update(table, row, where: 'recordID = ?', whereArgs: [id]);
+  }
+
 
   Future<int> delete(int id, String table) async {
     final dbHelper = DatabaseHelper.instance;
@@ -37,5 +44,25 @@ class DatabaseHelper {
     return await db.delete(table, where: 'recordID = ?' , whereArgs: [id]);
   }
 
+  Future<void> addColumn(String rowName, String table) async {
+    final dbHelper = DatabaseHelper.instance;
+    final db = await dbHelper.database;
+    bool doesExist = true;
+    final result = await db.rawQuery('PRAGMA table_info($table)');
+    for (var column in result) {
+      if (column['name'] != rowName) {
+        doesExist = false;
+      } else {
+        doesExist = true;
+        break;
+      }
+    }
+    if (!doesExist) {
+        await db.execute('''
+          ALTER TABLE $table ADD COLUMN $rowName;
+          ''');
+    }
+
+  }
 
 }
