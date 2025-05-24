df66f99626bdefb6c69ed1630a185586d3d586dd
BahresAtli <bahresatli@hotmail.com>
2025-05-24 20:28:11 +0300

    SQL Queries are updated and seperated

---
+++ lib/core/database/sql/create/mainTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE mainTable (
+    recordID INTEGER PRIMARY KEY,
+    startTime TEXT,
+    endTime TEXT,
+    elapsedMilisecs INTEGER,
+    startLatitude TEXT,
+    startLongitude TEXT,
+    endLatitude TEXT,
+    endLongitude TEXT,
+    label TEXT
+);
